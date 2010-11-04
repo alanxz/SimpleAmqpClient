b27c1f7e3190d29fde82744c4d4c6d180a804970
Alan Antonuk <alan.antonuk@gmail.com>
2010-11-04 13:20:39 -0400

    SimpleRpcServer implementation

--- SimpleRpcServer.cpp
+++ SimpleRpcServer.cpp
@@ -1,13 +1,42 @@
 #include "SimpleRpcServer.h"
 
+
 namespace AmqpClient {
 
-SimpleRpcServer::SimpleRpcServer()
+SimpleRpcServer::SimpleRpcServer(Channel::ptr_t channel, const std::string& rpc_name) :
+	m_channel(channel), m_incoming_tag(rpc_name)
+
 {
+	m_channel->DeclareQueue(m_incoming_tag);
+	m_channel->BindQueue(m_incoming_tag, "amq.direct", m_incoming_tag);
+	m_channel->BasicConsume(m_incoming_tag, m_incoming_tag);
 }
 
 SimpleRpcServer::~SimpleRpcServer()
 {
+}
+
+Message::ptr_t SimpleRpcServer::GetNextIncomingMessage()
+{
+	return m_channel->BasicConsumeMessage();
+}
+
+void SimpleRpcServer::RespondToMessage(Message::ptr_t request, Message::ptr_t response)
+{
+	if (request->CorrelationIdIsSet() && !response->CorrelationIdIsSet())
+	{
+		response->CorrelationId(request->CorrelationId());
+	}
+
+	m_channel->BasicPublish("amq.direct", request->ReplyTo(), response);
+}
+
+void SimpleRpcServer::RespondToMessage(Message::ptr_t request, const std::string response)
+{
+	Message::ptr_t response_message = Message::Create();
+	response_message->Body(response);
+
+	RespondToMessage(request, response_message);
 }
 
 } // namespace AmqpClient
--- SimpleRpcServer.h
+++ SimpleRpcServer.h
@@ -1,9 +1,13 @@
 #ifndef SIMPLERPCSERVER_H
 #define SIMPLERPCSERVER_H
 
+#include "Channel.h"
+#include "Message.h"
+
 #include <boost/utility.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
+#include <string>
 
 namespace AmqpClient {
 
@@ -13,18 +17,34 @@ class SimpleRpcServer : boost::noncopyable
 public:
 	typedef boost::shared_ptr<SimpleRpcServer> ptr_t;
 
-	friend ptr_t boost::make_shared<SimpleRpcServer>();
+	friend ptr_t boost::make_shared<SimpleRpcServer>(Channel::ptr_t const & a1, std::string const & a2);
 
-	static ptr_t Create() { return boost::make_shared<SimpleRpcServer>(); }
+	static ptr_t Create(Channel::ptr_t channel, const std::string& rpc_name) 
+		{ return boost::make_shared<SimpleRpcServer>(channel, rpc_name); }
+
+	static std::string CreateUniqueRpcName(const std::string prefix = "");
 
 private:
-	explicit SimpleRpcServer();
+	explicit SimpleRpcServer(Channel::ptr_t channel, const std::string& rpc_name);
 
 public:
 	virtual ~SimpleRpcServer();
 
+	std::string GetRpcName() const { return m_incoming_tag; }
+
+	Message::ptr_t GetNextIncomingMessage();
+
+	void RespondToMessage(Message::ptr_t request, 
+						  Message::ptr_t response);
+
+	void RespondToMessage(Message::ptr_t request,
+						  const std::string response);
 
 private:
+	Channel::ptr_t m_channel;
+	const std::string& m_incoming_tag;
+
+
 
 
 };
