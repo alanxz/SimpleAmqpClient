988dff6b88792369383fb061e70ac021d8051e7c
Alan Antonuk <alan.antonuk@gmail.com>
2010-11-04 12:18:53 -0400

    SimpleRpcClient Implementation

--- SimpleRpcClient.cpp
+++ SimpleRpcClient.cpp
@@ -1,13 +1,35 @@
 #include "SimpleRpcClient.h"
+#include "Message.h"
 
 namespace AmqpClient {
 
-SimpleRpcClient::SimpleRpcClient(Channel::ptr_t channel)
+SimpleRpcClient::SimpleRpcClient(Channel::ptr_t channel, const std::string&
+		rpc_name) :
+	m_channel(channel)
 {
+	// Declare the reply queue, by passing an empty string, the broker will
+	// give us a name
+	m_incoming_tag = m_channel->DeclareQueue("");
+	m_channel->BindQueue(m_incoming_tag, "amq.direct", m_incoming_tag);
+	m_channel->BasicConsume(m_incoming_tag, m_incoming_tag);
 }
 
 SimpleRpcClient::~SimpleRpcClient()
 {
+}
+
+std::string SimpleRpcClient::Call(const std::string& message)
+{
+	Message::ptr_t outgoing_msg;
+	outgoing_msg->Body(message);
+	outgoing_msg->ReplyTo(m_incoming_tag);
+
+	m_channel->BasicPublish("amq.direct", m_outgoing_tag, outgoing_msg);
+
+	m_channel->BasicConsume(m_incoming_tag, m_incoming_tag);
+
+	Message::ptr_t incoming_msg = m_channel->BasicConsumeMessage();
+	return incoming_msg->Body();
 }
 
 } // namespace AmqpClient
--- SimpleRpcClient.h
+++ SimpleRpcClient.h
@@ -6,6 +6,7 @@
 #include <boost/utility.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
+#include <string>
 
 namespace AmqpClient {
 
@@ -14,19 +15,25 @@ class SimpleRpcClient : boost::noncopyable
 {
 public:
 	typedef boost::shared_ptr<SimpleRpcClient> ptr_t;
-	friend ptr_t boost::make_shared<SimpleRpcClient>(Channel::ptr_t const & a1);
+	friend ptr_t boost::make_shared<SimpleRpcClient>(Channel::ptr_t const & a1, std::string const & a2);
 
-	static ptr_t Create(Channel::ptr_t channel) { return boost::make_shared<SimpleRpcClient>(channel); }
+	static ptr_t Create(Channel::ptr_t channel, const std::string& rpc_name) 
+	{ return boost::make_shared<SimpleRpcClient>(channel, rpc_name); }
 
 private:
-	explicit SimpleRpcClient(Channel::ptr_t channel);
+	explicit SimpleRpcClient(Channel::ptr_t channel, const std::string& rpc_name);
 
 public:
 	virtual ~SimpleRpcClient();
 
+	std::string Call(const std::string& message);
 
 
 private:
+	Channel::ptr_t m_channel;
+	const std::string m_outgoing_tag;
+	const std::string m_incoming_tag;
+
 
 };
 
