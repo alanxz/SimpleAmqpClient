b0164130d5e084e8d1246d07f0482f344ebc18ee
Alan Antonuk <aega@med.umich.edu>
2011-10-31 15:03:31 -0400

    Made test_connect cases expect correct exceptions

--- testing/test_connect.cpp
+++ testing/test_connect.cpp
@@ -15,16 +15,16 @@ TEST(connect_bad_host, connect)
 
 TEST(connect_bad_auth, connect)
 {
-  EXPECT_THROW(Channel::ptr_t channel = Channel::Create("localhost", 5672, "baduser", "badpass"), AmqpResponseServerException);
+  EXPECT_THROW(Channel::ptr_t channel = Channel::Create("localhost", 5672, "baduser", "badpass"), AmqpResponseLibraryException);
 }
 
 TEST(connect_bad_frame_size, connect)
 {
   // AMQP Spec says we have a minimum frame size of 4096
-  EXPECT_THROW(Channel::ptr_t channel = Channel::Create("localhost", 5672, "guest", "guest", "/", 400), AmqpResponseServerException);
+  EXPECT_THROW(Channel::ptr_t channel = Channel::Create("localhost", 5672, "guest", "guest", "/", 400), AmqpResponseLibraryException);
 }
 
 TEST(connect_bad_vhost, connect)
 {
-  EXPECT_THROW(Channel::ptr_t channel = Channel::Create("localhost", 5672, "guest", "guest", "nonexitant_vhost"), AmqpResponseServerException);
+  EXPECT_THROW(Channel::ptr_t channel = Channel::Create("localhost", 5672, "guest", "guest", "nonexitant_vhost"), AmqpResponseLibraryException);
 }
