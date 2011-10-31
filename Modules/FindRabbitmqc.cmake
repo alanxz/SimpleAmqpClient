1aa76e99ed4581a13f5ce5204fda1d0b67cf1eb5
Alan Antonuk <alan.antonuk@gmail.com>
2011-10-30 23:10:51 -0400

    Make Rabbitmq_DIR take precidence over system paths

--- Modules/FindRabbitmqc.cmake
+++ Modules/FindRabbitmqc.cmake
@@ -2,19 +2,19 @@
 
 INCLUDE(LibFindMacros)
 
-
 # Find the include directories
 FIND_PATH(Rabbitmqc_INCLUDE_DIR
 	NAMES amqp.h
-	PATHS ${Rabbitmqc_DIR}/include
+    HINTS ${Rabbitmqc_DIR}/include
 	)
 
 FIND_LIBRARY(Rabbitmqc_LIBRARY
 	NAMES rabbitmq
-	PATHS ${Rabbitmqc_DIR}/lib
+    HINTS ${Rabbitmqc_DIR}/lib
 	)
 
 SET(Rabbitmqc_PROCESS_INCLUDES Rabbitmqc_INCLUDE_DIR)
 SET(Rabbitmqc_PROCESS_LIBS Rabbitmqc_LIBRARY)
+
 LIBFIND_PROCESS(Rabbitmqc)
 
