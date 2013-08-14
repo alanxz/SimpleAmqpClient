5a2f64c94fe46d073433d86c459ea16c7905732e
Alan Antonuk <alan.antonuk@gmail.com>
2013-08-14 16:19:21 -0700

    Update Copyright date to include 2013

--- src/AmqpResponseLibraryException.cpp
+++ src/AmqpResponseLibraryException.cpp
@@ -2,7 +2,7 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Version: MIT
  *
- * Copyright (c) 2010-2012 Alan Antonuk
+ * Copyright (c) 2010-2013 Alan Antonuk
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
