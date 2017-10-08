a9ae7cdbc5272d0896bf98a5674be60abbbb212d
Juan Eugenio Abadie <abadiejuan@hotmail.com>
2017-10-07 23:22:48 -0300

    Remove unnecessary else statement

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -103,10 +103,9 @@ Channel::ptr_t Channel::CreateSecureFromUri(
                         path_to_client_key, path_to_client_cert, info.port,
                         std::string(info.user), std::string(info.password),
                         std::string(info.vhost), frame_max, verify_hostname);
-  } else {
-    throw std::runtime_error(
-        "CreateSecureFromUri only supports SSL-enabled URIs.");
   }
+  throw std::runtime_error(
+      "CreateSecureFromUri only supports SSL-enabled URIs.");
 }
 
 Channel::Channel(const std::string &host, int port, const std::string &username,
