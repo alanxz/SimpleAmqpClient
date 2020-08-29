94dd3fa78171f1c7ccd404ea3ca415548a466e49
Alan Antonuk <alan.antonuk@gmail.com>
2020-08-29 04:39:45 +0000

    lib: switch to StringToBytes from amqp_cstring_bytes
    The latter is somewhat more efficient as it doesn't need to call strlen,
    thus its O(1) instead of O(N) on string size. Its also a pre-req for
    moving to boost::string_ref

---
+++ src/SimpleAmqpClient/Bytes.h
@@ -0,0 +1,17 @@
+#ifndef SIMPLEAMQPCLIENT_BYTES_H
+#define SIMPLEAMQPCLIENT_BYTES_H
+
+#include <amqp.h>
+#include <string>
+
+namespace AmqpClient {
+
+amqp_bytes_t StringToBytes(const std::string& str) {
+  amqp_bytes_t ret;
+  ret.bytes = reinterpret_cast<void*>(const_cast<char*>(str.data()));
+  ret.len = str.length();
+  return ret;
+}
+
+}  // namespace AmqpClient
+#endif  // SIMPLEAMQPCLIENT_BYTES_H
