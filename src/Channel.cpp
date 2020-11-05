alanxz/SimpleAmqpClient

commit 938b102f8fc85e5113b3e55700b8af9b71d9f01d

lib: refactor BasicMessage removing rabbitmq-c API
This makes BasicMessage more value-type like, and removes forward
references to rabbitmq-c structs from the public API.

As a side-effect most accessor functions now return const-ref, which is
likely more efficient in most cases, though may cause an unexpected
behavior changes depending on how the API is used.

src/Channel.cpp
@@ -62,6 +62,83 @@
 
 namespace AmqpClient {
 
+namespace {
+
+amqp_bytes_t StringToBytes(const std::string &str) {
+  amqp_bytes_t ret;
+  ret.bytes = reinterpret_cast<void *>(const_cast<char *>(str.data()));
+  ret.len = str.length();
+  return ret;
+}
+
+amqp_basic_properties_t CreateAmqpProperties(const BasicMessage &mes,
+                                             Detail::amqp_pool_ptr_t &pool) {
+  amqp_basic_properties_t ret;
+  ret._flags = 0;
+
+  if (mes.ContentTypeIsSet()) {
+    ret.content_type = StringToBytes(mes.ContentType());
+    ret._flags |= AMQP_BASIC_CONTENT_TYPE_FLAG;
+  }
+  if (mes.ContentEncodingIsSet()) {
+    ret.content_encoding = StringToBytes(mes.ContentEncoding());
+    ret._flags |= AMQP_BASIC_CONTENT_ENCODING_FLAG;
+  }
+  if (mes.DeliveryModeIsSet()) {
+    // TODO: something more advanced?
+    ret.delivery_mode = mes.DeliveryMode();
+    ret._flags |= AMQP_BASIC_DELIVERY_MODE_FLAG;
+  }
+  if (mes.PriorityIsSet()) {
+    ret.priority = mes.Priority();
+    ret._flags |= AMQP_BASIC_PRIORITY_FLAG;
+  }
+  if (mes.CorrelationIdIsSet()) {
+    ret.correlation_id = StringToBytes(mes.CorrelationId());
+    ret._flags |= AMQP_BASIC_CORRELATION_ID_FLAG;
+  }
+  if (mes.ReplyToIsSet()) {
+    ret.reply_to = StringToBytes(mes.ReplyTo());
+    ret._flags |= AMQP_BASIC_REPLY_TO_FLAG;
+  }
+  if (mes.ExpirationIsSet()) {
+    ret.expiration = StringToBytes(mes.Expiration());
+    ret._flags = AMQP_BASIC_EXPIRATION_FLAG;
+  }
+  if (mes.MessageIdIsSet()) {
+    ret.message_id = StringToBytes(mes.MessageId());
+    ret._flags = AMQP_BASIC_MESSAGE_ID_FLAG;
+  }
+  if (mes.TimestampIsSet()) {
+    ret.timestamp = mes.Timestamp();
+    ret._flags = AMQP_BASIC_TIMESTAMP_FLAG;
+  }
+  if (mes.TypeIsSet()) {
+    ret.type = StringToBytes(mes.Type());
+    ret._flags = AMQP_BASIC_TYPE_FLAG;
+  }
+  if (mes.UserIdIsSet()) {
+    ret.user_id = StringToBytes(mes.UserId());
+    ret._flags = AMQP_BASIC_USER_ID_FLAG;
+  }
+  if (mes.AppIdIsSet()) {
+    ret.app_id = StringToBytes(mes.AppId());
+    ret._flags = AMQP_BASIC_APP_ID_FLAG;
+  }
+  if (mes.ClusterIdIsSet()) {
+    ret.cluster_id = StringToBytes(mes.ClusterId());
+    ret._flags = AMQP_BASIC_CLUSTER_ID_FLAG;
+  }
+  if (mes.HeaderTableIsSet()) {
+    ret.headers =
+        Detail::TableValueImpl::CreateAmqpTable(mes.HeaderTable(), pool);
+    ret._flags = AMQP_BASIC_HEADERS_FLAG;
+  }
+  return ret;
+}
+
+}  // namespace
+
 const std::string Channel::EXCHANGE_TYPE_DIRECT("direct");
 const std::string Channel::EXCHANGE_TYPE_FANOUT("fanout");
 const std::string Channel::EXCHANGE_TYPE_TOPIC("topic");
@@ -514,10 +591,13 @@ void Channel::BasicPublish(const std::string &exchange_name,
   m_impl->CheckIsConnected();
   amqp_channel_t channel = m_impl->GetChannel();
 
+  Detail::amqp_pool_ptr_t pool;
+  amqp_basic_properties_t properties = CreateAmqpProperties(*message, pool);
+
   m_impl->CheckForError(amqp_basic_publish(
       m_impl->m_connection, channel, amqp_cstring_bytes(exchange_name.c_str()),
       amqp_cstring_bytes(routing_key.c_str()), mandatory, immediate,
-      message->getAmqpProperties(), message->getAmqpBody()));
+      &properties, StringToBytes(message->Body())));
 
   // If we've done things correctly we can get one of 4 things back from the
   // broker

src/ChannelImpl.cpp
@@ -47,6 +47,7 @@
 #include "SimpleAmqpClient/ChannelImpl.h"
 #include "SimpleAmqpClient/ConnectionClosedException.h"
 #include "SimpleAmqpClient/ConsumerTagNotFoundException.h"
+#include "SimpleAmqpClient/TableImpl.h"
 #define BOOST_BIND_GLOBAL_PLACEHOLDERS
 #include <string.h>
 
@@ -57,6 +58,60 @@
 
 namespace AmqpClient {
 namespace Detail {
+
+namespace {
+
+std::string BytesToString(amqp_bytes_t bytes) {
+  return std::string(reinterpret_cast<char *>(bytes.bytes), bytes.len);
+}
+
+void SetMessageProperties(BasicMessage &mes,
+                          const amqp_basic_properties_t &props) {
+  if (0 != (props._flags & AMQP_BASIC_CONTENT_TYPE_FLAG)) {
+    mes.ContentType(BytesToString(props.content_type));
+  }
+  if (0 != (props._flags & AMQP_BASIC_CONTENT_ENCODING_FLAG)) {
+    mes.ContentEncoding(BytesToString(props.content_encoding));
+  }
+  if (0 != (props._flags & AMQP_BASIC_DELIVERY_MODE_FLAG)) {
+    mes.DeliveryMode(
+        static_cast<BasicMessage::delivery_mode_t>(props.delivery_mode));
+  }
+  if (0 != (props._flags & AMQP_BASIC_PRIORITY_FLAG)) {
+    mes.Priority(props.priority);
+  }
+  if (0 != (props._flags & AMQP_BASIC_CORRELATION_ID_FLAG)) {
+    mes.CorrelationId(BytesToString(props.correlation_id));
+  }
+  if (0 != (props._flags & AMQP_BASIC_REPLY_TO_FLAG)) {
+    mes.ReplyTo(BytesToString(props.reply_to));
+  }
+  if (0 != (props._flags & AMQP_BASIC_EXPIRATION_FLAG)) {
+    mes.Expiration(BytesToString(props.expiration));
+  }
+  if (0 != (props._flags & AMQP_BASIC_MESSAGE_ID_FLAG)) {
+    mes.MessageId(BytesToString(props.message_id));
+  }
+  if (0 != (props._flags & AMQP_BASIC_TIMESTAMP_FLAG)) {
+    mes.Timestamp(props.timestamp);
+  }
+  if (0 != (props._flags & AMQP_BASIC_TYPE_FLAG)) {
+    mes.Type(BytesToString(props.type));
+  }
+  if (0 != (props._flags & AMQP_BASIC_USER_ID_FLAG)) {
+    mes.UserId(BytesToString(props.user_id));
+  }
+  if (0 != (props._flags & AMQP_BASIC_APP_ID_FLAG)) {
+    mes.AppId(BytesToString(props.app_id));
+  }
+  if (0 != (props._flags & AMQP_BASIC_CLUSTER_ID_FLAG)) {
+    mes.ClusterId(BytesToString(props.cluster_id));
+  }
+  if (0 != (props._flags & AMQP_BASIC_HEADERS_FLAG)) {
+    mes.HeaderTable(TableValueImpl::CreateTable(props.headers));
+  }
+}
+}  // namespace
 
 ChannelImpl::ChannelImpl() : m_last_used_channel(0), m_is_connected(false) {
   m_channels.push_back(CS_Used);
@@ -233,11 +288,12 @@ BasicMessage::ptr_t ChannelImpl::ReadContent(amqp_channel_t channel) {
 
   GetNextFrameOnChannel(channel, frame);
 
-  if (frame.frame_type != AMQP_FRAME_HEADER)
+  if (frame.frame_type != AMQP_FRAME_HEADER) {
     // TODO: We should connection.close here
     throw std::runtime_error(
         "Channel::BasicConsumeMessage: received unexpected frame type (was "
         "expected AMQP_FRAME_HEADER)");
+  }
 
   // The memory for this is allocated in a pool associated with the connection
   // The BasicMessage constructor does a deep copy of the properties structure
@@ -252,7 +308,8 @@ BasicMessage::ptr_t ChannelImpl::ReadContent(amqp_channel_t channel) {
   size_t body_size = static_cast<size_t>(frame.payload.properties.body_size);
   size_t received_size = 0;
 
-  amqp_bytes_t body = amqp_bytes_malloc(body_size);
+  BasicMessage::ptr_t message = BasicMessage::Create();
+  message->Body().reserve(body_size);
 
   // frame #3 and up:
   while (received_size < body_size) {
@@ -264,12 +321,15 @@ BasicMessage::ptr_t ChannelImpl::ReadContent(amqp_channel_t channel) {
           "Channel::BasicConsumeMessage: received unexpected frame type (was "
           "expecting AMQP_FRAME_BODY)");
 
-    void *body_ptr = reinterpret_cast<char *>(body.bytes) + received_size;
-    memcpy(body_ptr, frame.payload.body_fragment.bytes,
-           frame.payload.body_fragment.len);
+    message->Body().append(
+        reinterpret_cast<char *>(frame.payload.body_fragment.bytes),
+        frame.payload.body_fragment.len);
     received_size += frame.payload.body_fragment.len;
   }
-  return BasicMessage::Create(body, properties);
+
+  SetMessageProperties(*message, *properties);
+
+  return message;
 }
 
 void ChannelImpl::CheckFrameForClose(amqp_frame_t &frame,

src/SimpleAmqpClient/BasicMessage.h
@@ -46,14 +46,7 @@
 /// @file SimpleAmqpClient/BasicMessage.h
 /// The AmqpClient::BasicMessage class is defined in this header file.
 
-struct amqp_bytes_t_;
-struct amqp_basic_properties_t_;
-
 namespace AmqpClient {
-
-namespace Detail {
-class BasicMessageImpl;
-}
 
 /**
  * An AMQP BasicMessage
@@ -64,7 +57,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   typedef boost::shared_ptr<BasicMessage> ptr_t;
 
   /// With durable queues, messages can be requested to persist or not
-  enum delivery_mode_t { dm_nonpersistent = 1, dm_persistent = 2 };
+  enum delivery_mode_t {
+    dm_notset = 0,
+    dm_nonpersistent = 1,
+    dm_persistent = 2
+  };
 
   /**
    * Create a new empty BasicMessage object
@@ -77,32 +74,14 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
    * @param body the message body.
    * @returns a new BasicMessage object
    */
-  static ptr_t Create(const std::string &body) {
+  static ptr_t Create(const std::string& body) {
     return boost::make_shared<BasicMessage>(body);
-  }
-
-  /**
-   * Create a new BasicMessage object with given body and properties
-   *
-   * @param body The message body. The message body is NOT copied.
-   * Passed in message body is deallocated when: body is set or message is
-   * destructed.
-   * @param properties The `amqp_basic_properties_t struct`. Note this makes a
-   * deep copy of the properties struct.
-   * @returns a new BasicMessage object
-   */
-  static ptr_t Create(amqp_bytes_t_ &body,
-                      amqp_basic_properties_t_ *properties) {
-    return boost::make_shared<BasicMessage>(body, properties);
   }
 
   /// Construct empty BasicMessage
   BasicMessage();
   /// Construct BasicMessage with given body
-  BasicMessage(const std::string &body);
-  /// Construct BasicMessage with given body and properties
-  BasicMessage(const amqp_bytes_t_ &body,
-               const amqp_basic_properties_t_ *properties);
+  BasicMessage(const std::string& body);
 
  public:
   /**
@@ -111,41 +90,24 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   virtual ~BasicMessage();
 
   /**
-   * INTERNAL INTERFACE: Gets the internal amqp_basic_properties_t struct
-   *
-   * Retrieves a reference to the internal structure used to keep track of
-   * the properties of the message. Changing members of this structure will
-   * modify the properties on the message.
-   * @returns the amqp_basic_properties_t* struct
-   */
-  const amqp_basic_properties_t_ *getAmqpProperties() const;
-
-  /**
-   * INTERNAL INTERFACE: Gets the amqp_bytes_t representation of the message
-   * body
-   *
-   * @returns the message body. Note this is owned by the message and will
-   * be freed when the BasicMessage is destructed
-   */
-  const amqp_bytes_t_ &getAmqpBody() const;
-
-  /**
    * Gets the message body as a std::string
    */
-  std::string Body() const;
+  const std::string& Body() const;
+  std::string& Body();
+
   /**
    * Sets the message body as a std::string
    */
-  void Body(const std::string &body);
+  void Body(const std::string& body);
 
   /**
    * Gets the content type property
    */
-  std::string ContentType() const;
+  const std::string& ContentType() const;
   /**
    * Sets the content type property
    */
-  void ContentType(const std::string &content_type);
+  void ContentType(const std::string& content_type);
   /**
    * Determines whether the content type property is set
    */
@@ -158,11 +120,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the content encoding property
    */
-  std::string ContentEncoding() const;
+  const std::string& ContentEncoding() const;
   /**
    * Sets the content encoding property
    */
-  void ContentEncoding(const std::string &content_encoding);
+  void ContentEncoding(const std::string& content_encoding);
   /**
    * Determines whether the content encoding property is set
    */
@@ -209,11 +171,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the correlation id property
    */
-  std::string CorrelationId() const;
+  const std::string& CorrelationId() const;
   /**
    * Sets the correlation id property
    */
-  void CorrelationId(const std::string &correlation_id);
+  void CorrelationId(const std::string& correlation_id);
   /**
    * Determines whether the correlation id property is set
    */
@@ -226,11 +188,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the reply to property
    */
-  std::string ReplyTo() const;
+  const std::string& ReplyTo() const;
   /**
    * Sets the reply to property
    */
-  void ReplyTo(const std::string &reply_to);
+  void ReplyTo(const std::string& reply_to);
   /**
    * Determines whether the reply to property is set
    */
@@ -243,11 +205,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the expiration property
    */
-  std::string Expiration() const;
+  const std::string& Expiration() const;
   /**
    * Sets the expiration property
    */
-  void Expiration(const std::string &expiration);
+  void Expiration(const std::string& expiration);
   /**
    * Determines whether the expiration property is set
    */
@@ -260,11 +222,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the message id property
    */
-  std::string MessageId() const;
+  const std::string& MessageId() const;
   /**
    * Sets the message id property
    */
-  void MessageId(const std::string &message_id);
+  void MessageId(const std::string& message_id);
   /**
    * Determines if the message id property is set
    */
@@ -294,11 +256,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the type property
    */
-  std::string Type() const;
+  const std::string& Type() const;
   /**
    * Sets the type property
    */
-  void Type(const std::string &type);
+  void Type(const std::string& type);
   /**
    * Determines whether the type property is set
    */
@@ -311,11 +273,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the user id property
    */
-  std::string UserId() const;
+  const std::string& UserId() const;
   /**
    * Sets the user id property
    */
-  void UserId(const std::string &user_id);
+  void UserId(const std::string& user_id);
   /**
    * Determines whether the user id property is set
    */
@@ -328,11 +290,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the app id property
    */
-  std::string AppId() const;
+  const std::string& AppId() const;
   /**
    * Sets the app id property
    */
-  void AppId(const std::string &app_id);
+  void AppId(const std::string& app_id);
   /**
    * Determines whether the app id property is set
    */
@@ -345,11 +307,11 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the cluster id property
    */
-  std::string ClusterId() const;
+  const std::string& ClusterId() const;
   /**
    * Sets the custer id property
    */
-  void ClusterId(const std::string &cluster_id);
+  void ClusterId(const std::string& cluster_id);
   /**
    * Determines if the cluster id property is set
    */
@@ -362,11 +324,12 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   /**
    * Gets the header table property
    */
-  Table HeaderTable() const;
+  Table& HeaderTable();
+  const Table& HeaderTable() const;
   /**
    * Sets the header table property
    */
-  void HeaderTable(const Table &header_table);
+  void HeaderTable(const Table& header_table);
   /**
    * Is there a header table associated with the message
    */
@@ -377,8 +340,9 @@ class SIMPLEAMQPCLIENT_EXPORT BasicMessage : boost::noncopyable {
   void HeaderTableClear();
 
  protected:
+  struct Impl;
   /// PIMPL idiom
-  boost::scoped_ptr<Detail::BasicMessageImpl> m_impl;
+  boost::scoped_ptr<Impl> m_impl;
 };
 
 }  // namespace AmqpClient

testing/test_message.cpp
@@ -109,21 +109,11 @@ TEST(basic_message, embedded_nulls) {
   BasicMessage::ptr_t message = BasicMessage::Create(body);
   EXPECT_EQ(body, message->Body());
 
-  amqp_bytes_t amqp_body = message->getAmqpBody();
-  EXPECT_EQ(body.length(), amqp_body.len);
-  EXPECT_TRUE(std::equal(message_data.begin(), message_data.end(),
-                         reinterpret_cast<char *>(amqp_body.bytes)));
-
   const boost::array<char, 7> message_data2 = {
       {'1', '2', '3', 0, 'a', 'b', 'c'}};
   const std::string body2(message_data2.data(), message_data2.size());
   message->Body(body2);
   EXPECT_EQ(body2, message->Body());
-
-  amqp_bytes_t amqp_body2 = message->getAmqpBody();
-  EXPECT_EQ(body2.length(), amqp_body2.len);
-  EXPECT_TRUE(std::equal(message_data2.begin(), message_data2.end(),
-                         reinterpret_cast<char *>(amqp_body2.bytes)));
 }
 
 TEST_F(connected_test, replaced_received_body) {

commit 4f3a249486d98baa9aabfe54d54f3bd1db4de011

Fix setting of flags in CreateAmqpProperties().
We cannot just replace the entire `ret._flags` variable when setting certain
message properties. We need to OR the variable with the corresponding flag.
Otherwise, setting e.g. message expiration resets all the previously set flags,
such as for priority or delivery mode. Many flags are ORed correctly (e.g.
priority or delivery mode), but some are not. This commit fixes that.

The bug that this commit fixes was introduced in commit
938b102f8fc85e5113b3e55700b8af9b71d9f01d.

src/Channel.cpp
@@ -97,36 +97,36 @@ amqp_basic_properties_t CreateAmqpProperties(const BasicMessage &mes,
   }
   if (mes.ExpirationIsSet()) {
     ret.expiration = StringToBytes(mes.Expiration());
-    ret._flags = AMQP_BASIC_EXPIRATION_FLAG;
+    ret._flags |= AMQP_BASIC_EXPIRATION_FLAG;
   }
   if (mes.MessageIdIsSet()) {
     ret.message_id = StringToBytes(mes.MessageId());
-    ret._flags = AMQP_BASIC_MESSAGE_ID_FLAG;
+    ret._flags |= AMQP_BASIC_MESSAGE_ID_FLAG;
   }
   if (mes.TimestampIsSet()) {
     ret.timestamp = mes.Timestamp();
-    ret._flags = AMQP_BASIC_TIMESTAMP_FLAG;
+    ret._flags |= AMQP_BASIC_TIMESTAMP_FLAG;
   }
   if (mes.TypeIsSet()) {
     ret.type = StringToBytes(mes.Type());
-    ret._flags = AMQP_BASIC_TYPE_FLAG;
+    ret._flags |= AMQP_BASIC_TYPE_FLAG;
   }
   if (mes.UserIdIsSet()) {
     ret.user_id = StringToBytes(mes.UserId());
-    ret._flags = AMQP_BASIC_USER_ID_FLAG;
+    ret._flags |= AMQP_BASIC_USER_ID_FLAG;
   }
   if (mes.AppIdIsSet()) {
     ret.app_id = StringToBytes(mes.AppId());
-    ret._flags = AMQP_BASIC_APP_ID_FLAG;
+    ret._flags |= AMQP_BASIC_APP_ID_FLAG;
   }
   if (mes.ClusterIdIsSet()) {
     ret.cluster_id = StringToBytes(mes.ClusterId());
-    ret._flags = AMQP_BASIC_CLUSTER_ID_FLAG;
+    ret._flags |= AMQP_BASIC_CLUSTER_ID_FLAG;
   }
   if (mes.HeaderTableIsSet()) {
     ret.headers =
         Detail::TableValueImpl::CreateAmqpTable(mes.HeaderTable(), pool);
-    ret._flags = AMQP_BASIC_HEADERS_FLAG;
+    ret._flags |= AMQP_BASIC_HEADERS_FLAG;
   }
   return ret;
 }