f3df9258fb33ffabcd76c883ba75f2e62e3fd30f
Prateek Chokse <prateek.chokse@gmail.com>
2021-03-30 13:49:12 +0530

    update rabbitmq package name to simplifying usage

--- Modules/FindRabbitmqc.cmake
+++ Modules/FindRabbitmqc.cmake
@@ -1,32 +1,33 @@
-#Find the Rabbitmq C library
+find_package(rabbitmq-c CONFIG REQUIRED)
+# needed because currently rabbitmq target doesn;t have include path in it
+get_filename_component(_Rabbitmqc_PARENT_DIR ${rabbitmq-c_DIR} DIRECTORY)
+set(_Rabbitmqc_INCLUDE_DIR "${_Rabbitmqc_PARENT_DIR}/include")
+while(NOT EXISTS ${_Rabbitmqc_INCLUDE_DIR})
+  get_filename_component(_Rabbitmqc_PARENT_DIR ${_Rabbitmqc_PARENT_DIR} DIRECTORY)
+  set(_Rabbitmqc_INCLUDE_DIR "${_Rabbitmqc_PARENT_DIR}/include")
+endwhile()
 
-INCLUDE(LibFindMacros)
-
-# Find the include directories
-FIND_PATH(Rabbitmqc_INCLUDE_DIR
-	NAMES amqp.h
-    HINTS ${Rabbitmqc_DIR}/include
-	)
-
-FIND_LIBRARY(Rabbitmqc_LIBRARY
-	NAMES rabbitmq
-    HINTS ${Rabbitmqc_DIR}/lib
-	)
-
-SET(Rabbitmqc_PROCESS_INCLUDES Rabbitmqc_INCLUDE_DIR)
-SET(Rabbitmqc_PROCESS_LIBS Rabbitmqc_LIBRARY)
-
-LIBFIND_PROCESS(Rabbitmqc)
-
+set(Rabbitmqc_INCLUDE_DIR ${_Rabbitmqc_INCLUDE_DIR})
+message(STATUS "asdasd ${Rabbitmqc_INCLUDE_DIR}")
 find_file(_Rabbitmqc_SSL_HEADER
   NAMES amqp_ssl_socket.h
   PATHS ${Rabbitmqc_INCLUDE_DIR}
   NO_DEFAULT_PATH
-  )
+)
 
 string(COMPARE NOTEQUAL "${_Rabbitmqc_SSL_HEADER}"
   "_Rabbitmqc_SSL_HEADER-NOTFOUND" _rmqc_ssl_enabled)
 
 set(Rabbitmqc_SSL_ENABLED ${_rmqc_ssl_enabled} CACHE BOOL
   "Rabbitmqc is SSL Enabled" FORCE)
+
+if(TARGET rabbitmq::rabbitmq)
+  set_target_properties(rabbitmq::rabbitmq PROPERTIES IMPORTED_GLOBAL TRUE)
+  add_library(Rabbitmqc::rabbitmq ALIAS rabbitmq::rabbitmq)
+else()
+  set_target_properties(rabbitmq::rabbitmq-static PROPERTIES IMPORTED_GLOBAL TRUE)
+  add_library(Rabbitmqc::rabbitmq ALIAS rabbitmq::rabbitmq-static)
+endif()
+
+mark_as_advanced(_Rabbitmqc_INCLUDE_DIR Rabbitmqc_INCLUDE_DIR)
 mark_as_advanced(_Rabbitmqc_SSL_HEADER Rabbitmqc_SSL_ENABLED)
